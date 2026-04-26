947f0697e49fcfd347c48889fcf243fe4f82206b
Yuanqin Zhao <zhao_yuanqin@163.com>
2026-04-26 21:32:40 +0800

    chore: remove non-core chapter1 assets and unrelated chapter2 content

--- chapter1_foundation/losses/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Losses for Chapter 1."""
-from .geodesic_loss import AnatomicalDistanceLoss, GeodesicAttentionLoss
-
-__all__ = ["AnatomicalDistanceLoss", "GeodesicAttentionLoss"]
--- chapter1_foundation/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Visualization utilities for Chapter 1."""
